133727b51ee8abed288ef522a105e685f882e529
Doc <elihpoehtmusic@gmail.com>
2025-06-11 23:59:46 +0200

    a bb is born

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import startAsciiAnimation from './lib/@cr1bl3-cli/animate.js';
 import { funkyConsoleLog } from './lib/@cr1bl3-cli/funkyConsoleLog.js';
-
+require('dotenv').config();
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
@@ -35,7 +35,16 @@ wss.on('connection', (ws) => {
   funkyConsoleLog('Client connected', ['yellow']);
   ws.on('close', () => funkyConsoleLog('Client disconnected', ['yellow']));
 });
-
+app.get('/firebase-config', (req, res) => {
+  res.json({
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+  });
+});
 server.listen(7410, () => {
   console.clear();
   startAsciiAnimation();
